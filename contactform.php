30412b891fc85835db1ad5bcc5da47005029b2a3
Darshan M Hegde <68365486+hegdes007@users.noreply.github.com>
2021-03-07 10:32:29 +0530

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -6,8 +6,9 @@
     $formcontent = "User Name: $name \n User Email: $email \n User Message: $message \n";
     $recipient = "hegdes007@rediffmail.com";
     $subject = "New form submission in Meditation-App";
-    $mailheader = "From: $email \r\n";
-    
+    $mailheader = "From: Mindfullness app \r\n";
+    $mailheader .= "Reply-To: $email \r\n";
+
     mail($recipient,$subject,$formcontent,$mailheader) or die("Error!");
     echo "Thank You!" ;
     
